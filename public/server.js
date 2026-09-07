ebf5727b93f55c54666853c89e5a83da119ffd44
Christian Kragh <eaa23chkr@students.eaaa.dk>
2026-09-07 21:24:56 +0200

    Fourth commit - logic added to ask-route, css updated and html updated so it can show question and answer

--- public/server.js
+++ public/server.js
@@ -1,5 +1,6 @@
 
 import express from "express";
+// import { messages } from "../messages.js";
 
 const app = express();
 const port = 3000;
@@ -8,14 +9,19 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+const messages = []
 
 app.get("/", (request, response) => {
-    response.render("index", { question: "" });
+    response.render("index", { messages });
 });
 
 app.post("/ask", (request, response) => {
     const question = request.body.question;
-    response.render("index", { question });
+
+    messages.push({ type: "question", text: "question" });
+    messages.push({ type: "answer", text: "Jeg leder efter et svar ..." })
+
+    response.render("index", { question, messages });
 });
 
 
